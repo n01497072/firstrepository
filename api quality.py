35385dfe4ed18411f3400b85be70f267a72f19f1
n01497072 <n01497072@humbermail.ca>
2022-06-02 16:20:02 -0400

    first try

---
+++ api quality.py
@@ -0,0 +1,8 @@
+import requests
+city = 'etobicoke'
+api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+response = requests.get(api_url, headers={'X-Api-Key': '3DmrUiA9RfyUmS1o/lBzdQ==xeFD7NjhoBZ4wyKW'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
