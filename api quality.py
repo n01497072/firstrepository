2ce5e3ffe314d257282f7d1320147bf33ad831cb
n01497072 <n01497072@humbermail.ca>
2022-06-02 16:56:57 -0400

    city

--- api quality.py
+++ api quality.py
@@ -1,5 +1,5 @@
 import requests
-city = 'jalandhar'
+city = 'toronto'
 api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
 response = requests.get(api_url, headers={'X-Api-Key': '3DmrUiA9RfyUmS1o/lBzdQ==xeFD7NjhoBZ4wyKW'})
 if response.status_code == requests.codes.ok:
